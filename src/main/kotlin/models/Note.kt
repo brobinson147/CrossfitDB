a66f196962d6d0934a0ee15653141f72da93a1f3
brobinson147 <benjrobinson1998@gmail.com>
2023-12-08 14:00:29 +0000

    Initial Commit

---
+++ src/main/kotlin/models/Note.kt
@@ -0,0 +1,7 @@
+package models
+
+data class Note (var noteTitle: String,
+                var notePriority: Int,
+                var noteCategory: String,
+                var isNoteArchived: Boolean){
+}
