3aa5501c083aea6bf2829d61c882769dac54ab9c
brobinson147 <benjrobinson1998@gmail.com>
2024-01-03 17:14:07 +0000

    Refactored main.kt to implement competitor and compete aswell as changed menu system and functions to suit new project needs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,35 @@
+// Main.kt
 import mu.KotlinLogging
 import persistence.JSONSerializer
-import utils.CategoryUtility
-import utils.ScannerInput
+import java.io.File
 import utils.ScannerInput.readNextInt
 import utils.ScannerInput.readNextLine
 import utils.ValidateInput.readValidCategory
 import utils.ValidateInput.readValidPriority
-import java.io.File
 import java.lang.System.exit
 
 private val logger = KotlinLogging.logger {}
-//private val noteAPI = NoteAPI(XMLSerializer(File("notes.xml")))
-private val competitorAPI = CompetitorAPI(JSONSerializer(File("notes.json")))
+private val competitorAPI = CompetitorAPI(JSONSerializer(File("competitors.json")))
 
 fun main(args: Array<String>) {
     runMenu()
 }
 
-fun mainMenu() : Int {
-    return ScannerInput.readNextInt(""" 
+fun mainMenu(): Int {
+    return readNextInt("""
          > -------------------------------------
-         > |          NOTE KEEPER APP          |
+         > |        CROSSFIT APP MAIN MENU      |
          > -------------------------------------
-         > | NOTE MENU                         |
-         > |   1) Add a note                   |
-         > |   2) List notes                   |
-         > |   3) Update a note                |
-         > |   4) Delete a note                |
-         > |   5) Archive a note               |
-         > |   6) Search note(by description)  |
+         > | MENU                               |
+         > |   1) Add Competitor                |
+         > |   2) List Competitors              |
+         > |   3) Add Competition               |
+         > |   4) List Competitions             |
          > -------------------------------------
-         > |   20) Save notes                  |
-         > |   21) Load notes                  |
+         > |   20) Save Data                    |
+         > |   21) Load Data                    |
          > -------------------------------------
-         > |   0) Exit                         |
+         > |   0) Exit                          |
          > -------------------------------------
          > ==>> """.trimMargin(">"))
 }
@@ -42,147 +38,64 @@ fun runMenu() {
     do {
         val option = mainMenu()
         when (option) {
-            1  -> addNote()
-            2  -> listNotes()
-            3  -> updateNote()
-            4  -> deleteNote()
-            5 -> archiveNote()
-            6 -> searchNotes()
-            20  -> save()
-            21  -> load()
-            0  -> exitApp()
+            1 -> addCompetitor()
+            2 -> listCompetitors()
+            3 -> addCompetition()
+            4 -> listCompetitions()
+            20 -> saveData()
+            21 -> loadData()
+            0 -> exitApp()
             else -> println("Invalid option entered: ${option}")
         }
     } while (true)
 }
 
-fun addCompetitor(){
-    //logger.info { "addNote() function invoked" }
-    val noteTitle = readNextLine("Enter a title for the note: ")
-    val notePriority = readValidPriority("Enter a priority (1-low, 2, 3, 4, 5-high): ")
-    val noteCategory = readValidCategory("Enter a category for the note from ${CategoryUtility.categories}: ")
-    val isAdded = noteAPI.add(Note(noteTitle, notePriority, noteCategory, false))
 
-    if (isAdded) {
-        println("Added Successfully")
+fun addCompetitor() {
+    val name = readNextLine("Enter competitor name: ")
+    val affiliation = readNextLine("Enter competitor affiliation: ")
+    val dob = readNextLine("Enter competitor date of birth: ")
+
+    val competitor = Competitor(lastCompetitorId(), name, affiliation, dob)
+    if (competitorAPI.addCompetitor(competitor)) {
+        println("Competitor added successfully.")
     } else {
-        println("Add Failed")
+        println("Failed to add competitor.")
     }
 }
 
-fun listNotes(){
-    if (noteAPI.numberOfNotes() > 0) {
-        val option = readNextInt(
-            """
-                  > --------------------------------
-                  > |   1) View ALL notes          |
-                  > |   2) View ACTIVE notes       |
-                  > |   3) View ARCHIVED notes     |
-                  > --------------------------------
-         > ==>> """.trimMargin(">"))
+fun listCompetitors() {
 
-        when (option) {
-            1 -> listAllNotes();
-            2 -> listActiveNotes();
-            3 -> listArchivedNotes();
-            else -> println("Invalid option entered: " + option);
-        }
-    } else {
-        println("Option Invalid - No notes stored");
-    }
 }
 
-fun listAllNotes() {
-    println(noteAPI.listAllNotes())
+fun addCompetition() {
+
 }
 
-fun listActiveNotes() {
-    println(noteAPI.listActiveNotes())
+fun listCompetitions() {
+
 }
 
-fun listArchivedNotes() {
-    println(noteAPI.listArchivedNotes())
-}
-
-fun updateNote() {
-    //logger.info { "updateNotes() function invoked" }
-    listNotes()
-    if (noteAPI.numberOfNotes() > 0) {
-        //only ask the user to choose the note if notes exist
-        val indexToUpdate = readNextInt("Enter the index of the note to update: ")
-        if (noteAPI.isValidIndex(indexToUpdate)) {
-            val noteTitle = readNextLine("Enter a title for the note: ")
-            val notePriority = readValidPriority("Enter a priority (1-low, 2, 3, 4, 5-high): ")
-            val noteCategory = readValidCategory("Enter a category for the note from ${CategoryUtility.categories}: ")
-
-            //pass the index of the note and the new note details to NoteAPI for updating and check for success.
-            if (noteAPI.updateNote(indexToUpdate, Note(noteTitle, notePriority, noteCategory, false))){
-                println("Update Successful")
-            } else {
-                println("Update Failed")
-            }
-        } else {
-            println("There are no notes for this index number")
-        }
-    }
-}
-
-fun deleteNote(){
-    //logger.info { "deleteNote() function invoked" }
-    listNotes()
-    if (noteAPI.numberOfNotes() > 0) {
-        //only ask the user to choose the note to delete if notes exist
-        val indexToDelete = readNextInt("Enter the index of the note to delete: ")
-        //pass the index of the note to NoteAPI for deleting and check for success.
-        val noteToDelete = noteAPI.deleteNote(indexToDelete)
-        if (noteToDelete != null) {
-            println("Delete Successful! Deleted note: ${noteToDelete.noteTitle}")
-        } else {
-            println("Delete NOT Successful")
-        }
-    }
-}
-
-fun archiveNote() {
-    listActiveNotes()
-    if (noteAPI.numberOfActiveNotes() > 0) {
-        //only ask the user to choose the note to archive if active notes exist
-        val indexToArchive = readNextInt("Enter the index of the note to archive: ")
-        //pass the index of the note to NoteAPI for archiving and check for success.
-        if (noteAPI.archiveNote(indexToArchive)) {
-            println("Archive Successful!")
-        } else {
-            println("Archive NOT Successful")
-        }
-    }
-}
-
-fun searchNotes() {
-    val searchTitle = readNextLine("Enter the description to search by: ")
-    val searchResults = noteAPI.searchByTitle(searchTitle)
-    if (searchResults.isEmpty()) {
-        println("No notes found")
-    } else {
-        println(searchResults)
-    }
-}
-fun save() {
+fun saveData() {
     try {
-        noteAPI.store()
+        competitorAPI.store()
     } catch (e: Exception) {
         System.err.println("Error writing to file: $e")
     }
 }
 
-fun load() {
+fun loadData() {
     try {
-        noteAPI.load()
+        competitorAPI.load()
     } catch (e: Exception) {
         System.err.println("Error reading from file: $e")
     }
 }
 
-fun exitApp(){
-    logger.info { "exitApp() function invoked" }
+fun exitApp() {
+    logger.info { "Exiting Application" }
     exit(0)
 }
+
+// Add the utility function lastCompetitorId() here
+
